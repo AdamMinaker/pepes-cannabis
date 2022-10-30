f3c0b0c7a50775ca8aeebc343cbf70ac3d2b064a
Adam <adamminaker@outlook.com>
2022-10-30 10:42:07 -0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_173439) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_150942) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "sku"
