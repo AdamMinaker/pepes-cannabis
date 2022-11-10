c2ea7be7779feae66052b8a06bf6cccba0ff581e
Adam <adamminaker@outlook.com>
2022-11-10 14:42:42 -0600

    Add product images to admin dashboard

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Product do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +13,37 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+  index do
+    column "Image" do |product|
+      image_tag product.image.variant(resize_to_fill: [50, 50]) if product.image.attached?
+    end
+    column "Name" do |product|
+      link_to product.name, admin_product_path(product)
+    end
+    column :description
+    column :category
+    column :stock
+    column :sku
+    # column :price
+    column "Price" do |product|
+      number_to_currency(product.price.fdiv(100))
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :category
+      row :description
+      row :sku
+      row :stock
+      row "Price" do |product|
+        number_to_currency(product.price.fdiv(100))
+      end
+      row :image do |product|
+        image_tag product.image.variant(resize_to_fill: [262, 200]) if product.image.attached?
+      end
+    end
+  end
 end
