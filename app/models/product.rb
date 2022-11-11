f3d8e9d7868607e96920a544b3468b60098405f2
Adam <adamminaker@outlook.com>
2022-11-11 11:31:52 -0600

    Add validations to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,6 @@ class Product < ApplicationRecord
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
   end
+  validates :name, presence: true
+  validates :price, :stock, numericality: { only_integer: true }
 end
